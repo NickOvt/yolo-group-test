70bd7a571df966a34aef1b65ff97cd2d9ca210e4
NickOvt <pukpikok1@gmail.com>
2023-07-05 23:23:39 +0300

    initial Spring Boot project setup, first commit

---
+++ src/main/java/org/yolotest/test/JavaTestYoloGroupApplication.java
@@ -0,0 +1,13 @@
+package org.yolotest.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestYoloGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTestYoloGroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/yolotest/test/JavaTestYoloGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yolotest.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestYoloGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
