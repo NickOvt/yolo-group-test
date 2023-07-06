1527dd0e85c8e8f0dc1013aa4f5d040023ace6e6
NickOvt <pukpikok1@gmail.com>
2023-07-06 20:40:18 +0300

    add initial websocket config

--- src/main/java/org/yolotest/test/dtos/GameRequestDto.java
+++ src/main/java/org/yolotest/test/dtos/GameRequestDto.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import lombok.Data;
 
 import java.math.BigDecimal;
 
